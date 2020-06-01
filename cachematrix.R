734bc2577b9382f1bd5b8586245c7fdcdf71c0da
bharatsenapati <55408117+bharatsenapati@users.noreply.github.com>
2020-06-01 20:11:30 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,17 +2,17 @@
 ## that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
-        set <- function(y) {
+        set <- function(y) {            ## Function to set the values
                 x <<- y
                 inv <<- NULL
         }
         get <- function() x  ## Returns the original matrix
-        setInverse <- function(inverse) inv <<- inverse
+        setInverse <- function(inverse) inv <<- inverse  
         getInverse <- function() inv
         list(set = set,
              get = get,
              setInverse = setInverse,
-             getInverse = getInverse)
+             getInverse = getInverse)    
 }
 
 ## This function computes the inverse of the special "matrix" 
@@ -27,6 +27,6 @@ cacheSolve <- function(x, ...) {
         }
         mat <- x$get()
         inv <- solve(mat, ...)  ## Creates the Inverse of the matrix
-        x$setInverse(inv)
-        inv
+        x$setInverse(inv)     
+        inv                     ## Last line of function so return invere when function is called
 }
